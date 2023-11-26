9d8731d98d4e4ecf6454e283c8f6c0155f8676be
atan <tanshiqi@gmail.com>
2023-11-26 11:45:37 +0800

    hide header

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -68,7 +68,7 @@
         </div>
     </div>
 
-    <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-800 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+    {{-- <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-800 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
         <button class="-m-2.5 p-2.5 text-gray-200 lg:hidden" type="button">
             <span class="sr-only">Open sidebar</span>
             <svg class="h-6 w-6" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -84,7 +84,7 @@
                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                  alt="">
         </a>
-    </div>
+    </div> --}}
 
     <main class="pb-64 pt-6 lg:pb-10 lg:pl-96 lg:pt-10">
         <div class="space-y-12 px-4 sm:px-6 lg:px-8">
