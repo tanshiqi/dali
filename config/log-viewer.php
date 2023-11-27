97b117b9332afc9dd85d0428fc1215fc9d627f8a
atan <tanshiqi@gmail.com>
2023-11-27 08:13:04 +0800

    exclude nginx logs

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -134,16 +134,16 @@ return [
         '**/*.log',
 
         // You can include paths to other log types as well, such as apache, nginx, and more.
-        '/var/log/httpd/*',
-        '/var/log/nginx/*',
+        // '/var/log/httpd/*',
+        // '/var/log/nginx/*',
 
         // MacOS Apple Silicon logs
-        '/opt/homebrew/var/log/nginx/*',
-        '/opt/homebrew/var/log/httpd/*',
-        '/opt/homebrew/var/log/php-fpm.log',
-        '/opt/homebrew/var/log/postgres*log',
-        '/opt/homebrew/var/log/redis*log',
-        '/opt/homebrew/var/log/supervisor*log',
+        // '/opt/homebrew/var/log/nginx/*',
+        // '/opt/homebrew/var/log/httpd/*',
+        // '/opt/homebrew/var/log/php-fpm.log',
+        // '/opt/homebrew/var/log/postgres*log',
+        // '/opt/homebrew/var/log/redis*log',
+        // '/opt/homebrew/var/log/supervisor*log',
 
         // '/absolute/paths/supported',
     ],
