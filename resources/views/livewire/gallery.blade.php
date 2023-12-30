943dee27712485d34d36d28915555a074a6a2635
atan <tanshiqi@gmail.com>
2023-12-30 12:05:33 +0800

    wip

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -1,8 +1,8 @@
-<div class="mx-auto max-w-7xl px-4 py-6">
+<div class="mx-auto max-w-7xl px-4 py-6" id="lightgallery">
 
     @foreach ($gallery as $date => $items)
         <h3 class="mb-2 font-bold text-white">{{ $date }}</h3>
-        <div class="mb-4 grid grid-cols-4 gap-0.5 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10" id="lightgallery" wire:poll.30s>
+        <div class="mb-4 grid grid-cols-4 gap-0.5 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10" wire:poll.30s>
             @foreach ($items as $photo)
                 <a href="{{ Storage::disk('qiniu')->url($photo->result) }}" title="{{ $photo->prompt }}">
                     <img class="" src="{{ Storage::disk('qiniu')->url($photo->result) }}?imageView2/1/w/300/format/jpg">
