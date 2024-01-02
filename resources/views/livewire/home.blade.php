1ada49c268285ad85c5939667233fa96637aa80e
atan <tanshiqi@gmail.com>
2024-01-02 18:53:32 +0800

    wip

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -143,7 +143,7 @@
         </div>
     </div>
 
-    <div class="sticky top-0 z-40 flex items-center gap-x-6 border-b border-gray-800 bg-[#2f3543] px-4 py-3 sm:px-6 lg:hidden">
+    <div class="sticky top-0 z-40 flex items-center gap-x-3 border-b border-gray-800 bg-[#2f3543] px-4 py-3 sm:px-6 lg:hidden">
         <div class="flex flex-1 items-center gap-3 text-sm font-semibold leading-6 text-gray-200">
             <svg class="h-7" aria-hidden="true" viewBox="0 0 32 32" stroke="currentColor" stroke-width="1.5" fill="none">
                 <path id="b"
@@ -153,7 +153,7 @@
         <livewire:components.ai-selector wire:model='aimodel' />
         <button class="flex items-center rounded-md text-sm text-white/60 lg:hidden" type="button" wire:click='quit'
                 wire:confirm="您可以收藏当前的地址随时继续您的创作，确定现在要退出吗？">
-            退出<svg class="ml-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+            <svg class="ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
                 <path fill-rule="evenodd"
                       d="M2 4.75A2.75 2.75 0 0 1 4.75 2h3a2.75 2.75 0 0 1 2.75 2.75v.5a.75.75 0 0 1-1.5 0v-.5c0-.69-.56-1.25-1.25-1.25h-3c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h3c.69 0 1.25-.56 1.25-1.25v-.5a.75.75 0 0 1 1.5 0v.5A2.75 2.75 0 0 1 7.75 14h-3A2.75 2.75 0 0 1 2 11.25v-6.5Zm9.47.47a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 1 1-1.06-1.06l.97-.97H5.25a.75.75 0 0 1 0-1.5h7.19l-.97-.97a.75.75 0 0 1 0-1.06Z"
                       clip-rule="evenodd" />
