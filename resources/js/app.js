705b5bee2084d88d49403bbda03b1bfa6ff2b4cf
atan <tanshiqi@gmail.com>
2024-01-04 14:16:31 +0800

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import "preline";
+
+document.addEventListener("alpine:init", () => {
+    Alpine.magic("clipboard", () => {
+        return (subject) => navigator.clipboard.writeText(subject);
+    });
+});
--- resources/views/livewire/viewer.blade.php
+++ resources/views/livewire/viewer.blade.php
@@ -2,7 +2,7 @@
     <div class="flex h-full w-full cursor-pointer items-center justify-center overflow-hidden p-1 lg:p-4" wire:click="$dispatch('closeModal')">
         <img class="max-h-full max-w-full rounded-md object-contain" src="{{ Storage::disk('qiniu')->url($task->result) . '?imageView2/0/w/1000/format/jpg' }}">
     </div>
-    <div class="lg:max-w-96 flex max-h-full flex-shrink-0 flex-col justify-between bg-gray-900/60 p-4 text-gray-300 lg:p-7">
+    <div class="lg:max-w-96 flex max-h-full flex-shrink-0 flex-col justify-between bg-gray-900/50 p-4 text-gray-300 lg:p-7">
         <div class="flex flex-1 flex-col gap-y-4">
             <div class="hidden items-center justify-end lg:flex">
                 <button class="rounded-full p-2 hover:bg-gray-800 focus-visible:outline-none" type="button" wire:click="$dispatch('closeModal')">
@@ -11,7 +11,7 @@
                     </svg>
                 </button>
             </div>
-            <p class="text-sm lg:text-base">{{ $task->prompt }}</p>
+            <p class="text-sm font-medium lg:text-base">{{ $task->prompt }}</p>
             <div
                  class="*:inline-flex *:items-center *:rounded-md *:bg-gray-400/10 *:px-2 *:py-1 *:text-xs *:font-medium *:text-gray-400 *:ring-1 *:ring-inset *:ring-gray-400/20 *:whitespace-nowrap *:mr-0.5 *:mb-0.5">
                 <span>{{ 'Sampler ' . data_get($task->sdparams, 'sampler_name') }}</span>
@@ -23,7 +23,8 @@
 
         <div class="mt-6 flex flex-shrink-0 items-center justify-between gap-x-2 lg:gap-x-4">
             <button class="rounded-md bg-white/10 px-3.5 py-2 text-xs font-semibold text-gray-300 shadow-sm hover:bg-white/20 focus:border-0 focus:ring-0 focus-visible:outline-none lg:w-1/2 lg:py-2.5 lg:text-sm"
-                    type="button">复制提示词</button>
+                    type="button" x-data="{ btntext: '复制提示词' }" @click="$clipboard('{{ $task->prompt }}');btntext='　已复制　'" x-text="btntext"
+                    x-effect="if(btntext != '复制提示词') setTimeout(() => btntext='复制提示词',1500)"></button>
             <a class="block rounded-md bg-white/10 px-3.5 py-2 text-center text-xs font-semibold text-gray-300 shadow-sm hover:bg-white/20 focus:border-0 focus:ring-0 focus-visible:outline-none lg:w-1/2 lg:py-2.5 lg:text-sm"
                type="button" href="{{ Storage::disk('qiniu')->url($task->result) . '?attname=' . Str::random(12) . '.png' }}">下载原图</a>
             <div class="flex-auto text-right">
