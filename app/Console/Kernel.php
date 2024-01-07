d6732e980c68cc45d78125bcb578156b82eefaac
atan <tanshiqi@gmail.com>
2024-01-07 19:03:28 +0800

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('task:check')->everyMinute();
+        $schedule->command('task:check')->everyFiveMinutes();
     }
 
     /**
